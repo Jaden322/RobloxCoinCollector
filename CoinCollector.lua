1b61f32fee96658153e8d883ec3dad2dc1998d2a
Jaden322 <66339644+Jaden322@users.noreply.github.com>
2026-01-01 23:24:38 -0800

    Create CoinCollector.lua

---
+++ CoinCollector.lua
@@ -0,0 +1,23 @@
+-- Coin Collector Script
+-- This script gives players 10 coins when they touch a coin object
+
+-- Step 1: Reference the coin in the game
+local coin = script.Parent  -- the coin object this script is attached to
+
+-- Step 2: Function to give coins
+local function onTouch(player)
+    -- Make sure a player touched it
+    if player and player:FindFirstChild("leaderstats") then
+        local coins = player.leaderstats:FindFirstChild("Coins")
+        if coins then
+            coins.Value = coins.Value + 10  -- add 10 coins
+        end
+    end
+    coin:Destroy()  -- remove the coin so it can't be collected again
+end
+
+-- 3 Connect the function to the touch event
+coin.Touched:Connect(function(hit)
+    local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+    onTouch(player)
+end)
